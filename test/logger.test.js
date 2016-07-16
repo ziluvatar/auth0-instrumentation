5683abd77e0a964480ff6cc9b025304b0cb301c7
Yamil Asusta <hello@yamilasusta.com>
2016-07-16 19:17:56 -0300

    Use spies for tests

--- test/logger.test.js
+++ test/logger.test.js
@@ -1,37 +1,26 @@
 'use strict';
 
 const assert = require('assert');
-const spy = require('sinon').spy;
-const $require = require('mockuire')(module);
-
-const spyException = spy();
-const spyMessage = spy();
-const logger = $require('../lib/logger', {
-  './error_reporter': function() {
-    return {
-      captureException: spyException,
-      captureMessage: spyMessage
-    }
-  }
-})({ name: 'test' }, { LOG_LEVEL: 'fatal' });
+const sentry = require('../lib/error_reporter')({}, {});
+const logger = require('../lib/logger')({ name: 'test' }, { LOG_LEVEL: 'fatal' });
 
 describe('logger', function() {
   beforeEach(function() {
-    spyException.reset();
-    spyMessage.reset();
+    sentry.captureException.reset();
+    sentry.captureMessage.reset();
   });
 
   describe('SentryStream', function() {
     it('should call captureException on error when level is error', function() {
       logger.error(new Error('test'));
-      assert(spyException.calledOnce);
-      assert(spyMessage.calledOnce === false);
+      assert(sentry.captureException.calledOnce);
+      assert(sentry.captureMessage.calledOnce === false);
     });
 
     it('should call captureMessage on string when level is error', function() {
       logger.error('test');
-      assert(spyException.calledOnce === false);
-      assert(spyMessage.calledOnce);
+      assert(sentry.captureException.calledOnce === false);
+      assert(sentry.captureMessage.calledOnce);
     });
   });
 });
