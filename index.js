1fb3ce62542c42542cf9e4508f51de2349320225
Yamil Asusta <hello@yamilasusta.com>
2017-07-07 13:34:05 -0400

    Add Profiler & cleanup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var stubs = require('./lib/stubs');
 var Logger = require('./lib/logger');
 var ErrorReporter = require('./lib/error_reporter');
 var Metrics = require('./lib/metrics');
+var Profiler = require('./lib/profiler');
 
 module.exports = {
   logger: stubs.logger,
@@ -12,5 +13,6 @@ module.exports = {
     this.logger = Logger(pkg, env, serializers);
     this.errorReporter = ErrorReporter(pkg, env);
     this.metrics = Metrics(pkg, env);
+    this.profiler = new Profiler(this, pkg, env);
   }
 };
